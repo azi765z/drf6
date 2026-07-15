0dc3541be2d54e3ff875ec1168ab1513de2ba462
aabduraximov2127-art <aabduraximov2127@gmail.com>
2026-07-15 13:53:16 +0500

    frist commit1

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path,include
+from . import views
+
 from django.urls import path
-from .views import ProductListView
+from . import views
 
 urlpatterns = [
-    path('products/', ProductListView.as_view({'get': 'list','post': 'create'}), name='product-list'),
+    path('products/', views.ProductListView.as_view()),
+    path('products/create/', views.Postcreate.as_view()),
+    path('products/update/<int:pk>/', views.PostUpdate.as_view()),
+    path('products/delete/<int:pk>/', views.ProductDelete.as_view()),
+    path('products/<int:pk>/', views.ProductsUpdateDestroyAPIView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializer import ProductSerializer
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
 # Create your views here.
 
-class ProductListView(viewsets.ModelViewSet):
+class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    
+class Postcreate(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class PostUpdate(RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+class ProductsUpdateDestroyAPIView(RetrieveDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+
+    
